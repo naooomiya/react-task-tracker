4a36da461a6e0b6ec28f23eba20f8e5344fc7989
naooomiya <hanwu.me@outlook.com>
2021-04-05 21:18:13 +1200

    http request and router

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,6 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
-    const onClick = (e) => {
-        console.log('Click')
-    }
     return (
         <header className='header'>
             <h1>{title}</h1>
